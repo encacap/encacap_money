1d26fb90a93d7626c6a16e39e2dd9d9640ec6113
Nguyen Khac Khanh <encacap@gmail.com>
2021-08-27 16:06:14 +0700

    Install precss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-    plugins: [require("postcss-import"), require("tailwindcss"), require("autoprefixer"), require("cssnano")],
+    plugins: [
+        // require("postcss-import"),
+        require("tailwindcss"),
+        require("precss"),
+        require("autoprefixer"),
+        require("cssnano"),
+    ],
 };
