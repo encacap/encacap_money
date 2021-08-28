0a6ad9fb4f7ad96ec8d379d97ec9de649e7f0408
Nguyen Khac Khanh <encacap@gmail.com>
2021-08-28 13:41:27 +0700

    Refactor to use SCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     plugins: [
-        // require("postcss-import"),
+        require("postcss-import"),
+        require("postcss-advanced-variables"),
+        require("postcss-nested"),
         require("tailwindcss"),
-        require("precss"),
         require("autoprefixer"),
         require("cssnano"),
     ],
