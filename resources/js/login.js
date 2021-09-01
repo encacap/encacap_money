28cee65de35111825ef697c72802a4316b36d1ce
Nguyen Khac Khanh <encacap@gmail.com>
2021-09-01 07:56:58 +0700

    Complete login page

--- resources/js/login.js
+++ resources/js/login.js
@@ -2,6 +2,8 @@ import Form from "./form";
 import axios from "axios";
 
 (() => {
+    window.localStorage.removeItem("userConfigs");
+
     const form = new Form("#login", {
         autofocus: "#username",
     });
@@ -30,16 +32,47 @@ import axios from "axios";
 
         try {
             const response = await axios.post("http://localhost:3000/auth/login", body);
+            const { data } = response.data;
+            const { token, refreshToken } = data.attributes;
+
+            window.localStorage.setItem(
+                "userConfigs",
+                JSON.stringify({
+                    token,
+                    refreshToken,
+                })
+            );
+
+            window.location.href = "/";
         } catch (error) {
             if (error.response) {
-                const { status, data } = error.response;
-                if (status === 401) {
-                    form.showError("Tên đăng nhập hoặc mật khẩu không chính xác.");
+                const { data } = error.response;
+
+                if (data) {
+                    const [{ code, source }] = data.errors;
+                    let inputFocusSelector = "";
+
+                    if (code === "ERR_AUTH_NOT_EXISTS") {
+                        form.showError("#username", "Tên đăng nhập không tồn tại");
+                        inputFocusSelector = "#username";
+                    } else if (code === "ERR_AUTH_INCORECT") {
+                        if (source.pointer.includes("username")) {
+                            inputFocusSelector = "#username";
+                        } else {
+                            inputFocusSelector = "#password";
+                        }
+                        form.showError("Tên đăng nhập hoặc mật khẩu không chính xác.");
+                    } else {
+                        form.showError("Đã xảy ra lỗi không xác định. Vui lòng thử lại sau!");
+                        return;
+                    }
+
+                    form.hideLoading();
                     form.abled();
-                } else if (status === 404) {
-                    form.showError("#username", "Tên đăng nhập không tồn tại.");
+                    form.focus(inputFocusSelector);
+                } else {
+                    form.showError("Đã xảy ra lỗi không xác định. Vui lòng thử lại sau!");
                 }
-                console.log("data", data);
             }
         }
     };
