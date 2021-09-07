d7f120e77a97fcc2c7adc2ce57d3bf1cd8f45c5d
Nguyen Khac Khanh <encacap@gmail.com>
2021-09-07 16:11:14 +0700

    Complete JS to show expensives

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,16 +11,82 @@ import API from "./configs/api";
         headers: requestHeaders,
     };
 
+    const getWeekNumber = (date) => {
+        const oneJan = new Date(date.getFullYear(), 0, 1);
+        const numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000));
+        return Math.ceil((date.getDay() + 1 + numberOfDays) / 7);
+    };
+
+    const getSunday = (date) => {
+        const dayOfWeek = date.getDay() || 7;
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const DAYS_IN_WEEK = 7;
+        return new Date(year, month, date.getDate() - dayOfWeek + DAYS_IN_WEEK);
+    };
+
+    const render = (data) => {
+        const container = document.querySelector("#container");
+        let totalAmount = 0;
+
+        container.innerHTML = ` 
+            <div class="w-full border-2 border-gray-100 rounded-lg px-3 py-5">
+                <div class="mb-2 px-3">
+                    <span
+                        class="
+                            text-xs
+                            font-semibold
+                            inline-block
+                            py-1
+                            px-2
+                            rounded-full
+                            text-indigo-600
+                            bg-indigo-200
+                            uppercase
+                            last:mr-0
+                            mr-1
+                        "
+                    >
+                        2021
+                    </span>
+                    <h3 class="mt-2 font-semibold text-indigo-600">Tuần 36: 06/09 - 12/09</h3>
+                </div>
+                <ul class="border-t-2 border-transparent py-2">
+                ${data
+                    .map((item) => {
+                        const { attributes } = item;
+                        const { paymentDate, name, amount } = attributes;
+                        totalAmount += amount;
+                        return `
+                            <li class="px-3 py-1 hover:bg-gray-100 hover:cursor-pointer rounded-md">
+                                <span class="font-semibold">09</span> - ${name}: ${amount}
+                            </li>
+                        `;
+                    })
+                    .join("")}
+                </ul>
+                <h4 class="pt-2 px-3 font-semibold text-indigo-600">Tổng cộng: ${totalAmount}</h4>
+            </div>
+        `;
+    };
+
     let payments;
 
+    const currentDate = new Date();
+    const currentWeekNumber = getWeekNumber(currentDate);
+    const currentSunday = getSunday(currentDate);
+
+    const dateFrom = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
+    const dateTo = `${currentSunday.getFullYear()}-${currentSunday.getMonth() + 1}-${currentSunday.getDate()}`;
+
     try {
-        const response = await axios.get(`${API.gateway}/money/encacap/`, requestConfigs);
+        const response = await axios.get(`${API.gateway}/money/${dateFrom}/${dateTo}`, requestConfigs);
         payments = response.data;
     } catch (error) {
-        console.log(error);
+        // console.log(error);
     }
 
-    console.log(payments);
+    render(payments.data);
 
     // Add expenses modal
 
@@ -54,7 +120,9 @@ import API from "./configs/api";
     addExpensesClose.addEventListener("click", hideModal);
     addExpensesButton.addEventListener("click", showModal);
 
-    addExpensesForm.onsubmit = () => {
+    addExpensesForm.onsubmit = async (e) => {
+        e.preventDefault();
+
         const name = expensesName.value;
         const amount = expensesAmount.value;
         const date = expensesDate.value;
@@ -64,16 +132,25 @@ import API from "./configs/api";
             return false;
         }
 
-        const newExpense = {
-            name,
-            amount,
-            date,
-        };
-
-        localStorage.setItem("expenses", JSON.stringify(newExpense));
-        console.log(newExpense);
+        try {
+            await axios.post(
+                `${API.gateway}/money`,
+                {
+                    data: {
+                        attributes: {
+                            name,
+                            amount,
+                            paymentDate: date,
+                        },
+                    },
+                },
+                requestConfigs
+            );
+        } catch (error) {
+            console.log(error);
+        }
 
         hideModal();
-        return newExpense;
+        return undefined;
     };
 })();
